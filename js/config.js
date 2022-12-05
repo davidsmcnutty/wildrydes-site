d96d1bad7b781b7dff63958fff67ad9cb1e70bd8
davidsmcnutty <51972302+davidsmcnutty@users.noreply.github.com>
2022-12-04 21:19:45 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId:         '',         // e.g. us-east-2_uXboG5pAb
-        userPoolClientId:   '',         // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region:             ''          // e.g. us-east-2
+        userPoolId: 'us-east-1_2HkQiEgQj', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1m6hoqsm4ic8gcvla3oeodgpua', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-2' // e.g. us-east-2
     },
     api: {
-        invokeUrl:          ''          // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
